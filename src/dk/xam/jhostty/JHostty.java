b60c70f640652a13f07b848995663bf475a46255
Max Rydahl Andersen <max@xam.dk>
2026-07-03 16:10:21 +0200

    Debug: add removeTerminal stacktrace to diagnose drag-and-drop pane disappearance

--- src/dk/xam/jhostty/JHostty.java
+++ src/dk/xam/jhostty/JHostty.java
@@ -689,6 +689,8 @@ public class JHostty extends Application {
     }
 
     static void removeTerminal(TerminalView view, TabPane tabPane, Stage stage) {
+        debug("removeTerminal: " + view.getTitle());
+        new Exception("removeTerminal").printStackTrace(System.err);
         if (!closingTerminals.add(view)) return;
         Thread.ofVirtual().name("jhostty-close").start(view::close);
 
